3e465ec8924e995ceeb5c1f1409bc0b50269a2ff
peterthepage <mastermind723.pp@gmail.com>
2022-06-27 15:40:20 -0500

    stuff

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ def welcome():
     ''')
 @app.route('/api/v1.0/precipitation')
 def precipitation():
-    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-        precipiation = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date >= prev_year).all()
-        precip = {date: prcp for date, prcp in precipitation}
-    return jsonify(precip)
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
